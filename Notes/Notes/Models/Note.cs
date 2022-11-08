0f51fb80e44fdf4f50895e94ed3381b5bf78709f
cassidybrook <92697669+cassidybrook@users.noreply.github.com>
2022-11-09 10:28:56 +1300

    Add project files.

---
+++ Notes/Notes/Models/Note.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public string Ingredients { get; set; }
+        public string Title { get; set; }
+
+        public string Steps { get; set; }   
+        public DateTime Date { get; set; }
+    }
+}
